30c615830820ca87a097413afa93b11c4980c51a
SamanyuCodes <108064618+SamanyuCodes@users.noreply.github.com>
2022-06-23 16:24:18 +0530

    Javascript Code
    The code for Javascript.

---
+++ javascript.js
@@ -0,0 +1,6 @@
+            let money = 0;
+            document.getElementById("earn").onclick = function() 
+            {
+                money++;
+                document.getElementById("cash").innerHTML = money + "$";
+            }
